a83acc3042fee2353415caa77fdbbaa45a8bd4e5
Odzen <jsebastian.va@gmail.com>
2022-10-20 13:20:06 -0500

    Proccessing files in server

--- server/types/file.go
+++ server/types/file.go
@@ -4,9 +4,24 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 const SIZE = 1024
+
+type File struct {
+	Name    string
+	Size    int
+	Content []byte
+}
+
+func NewFile(name string, size int, content []byte) File {
+	return File{
+		Name:    name,
+		Size:    size,
+		Content: content,
+	}
+}
 
 func SendFileToClient(connection net.Conn, path string) {
 	fmt.Println("Sending File to client", connection.LocalAddr().String())
@@ -18,16 +33,23 @@ func SendFileToClient(connection net.Conn, path string) {
 		fmt.Println("Error reading the file:", err)
 		return
 	}
-	defer file.Close()
+	//defer file.Close()
 
 	// file's data can be read into a slice of bytes
 	data := make([]byte, 100)
 	count, err := file.Read(data)
 	if err != nil {
 		fmt.Println("Error counting the bytes of the file:", err)
+		return
 	}
 
 	fmt.Printf("read %d bytes: %q\n", count, data[:count])
+
+	newFile := NewFile(strings.Split(file.Name(), "/")[1], count, data[:count])
+	fmt.Printf("File to send over: %s -- %d -- %q", newFile.Name, newFile.Size, newFile.Content)
+
+	// fileInfo, err := file.Stat()
+	// fmt.Println("File information: ", fileInfo)
 
 	//size := fillString()
 }
